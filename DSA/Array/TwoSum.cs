f6407596320370e5b23a8cf540e89d441bc9014c
vu.truongp <vu.truongp@saigontechnology.com>
2025-06-05 23:29:00 +0700

    Two sum blind 75

---
+++ DSA/Array/TwoSum.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.Array;
+public class Solution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        for (var i = 0; i < nums.Length; i++)
+        {
+            for (var y = i + 1; y < nums.Length; y++)
+            {
+                if ((nums[i] + nums[y]) == target)
+                    return new int[] { i, y };
+            }
+        }
+
+        return new int[] { };
+    }
+
+    public int[] TwoSum2(int[] nums, int target)
+    {
+
+        var map = new Dictionary<int, int>(); //number - index
+        for (var i = 0; i < nums.Length; i++)
+        {
+            var complement = target - nums[i];
+            if (map.ContainsKey(complement))
+            {
+                return new int[] { map[complement], i };
+            }
+
+            if (!map.ContainsKey(complement))
+            {
+                map[nums[i]] = i;
+            }
+        }
+
+        return new int[] { };
+    }
+}
+
+//https://leetcode.com/problems/two-sum/description/
