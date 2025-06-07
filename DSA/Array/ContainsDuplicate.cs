028fa477e19dd7402d3cefc6cf06e07846ac000b
vu.truongp <vu.truongp@saigontechnology.com>
2025-06-07 17:37:31 +0700

    Contains Duplicate

---
+++ DSA/Array/ContainsDuplicate.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.Array;
+internal class ContainsDuplicate
+{
+    public bool ContainsDuplicate_v1(int[] nums)
+    {
+        var map = new Dictionary<int, int>();
+        for (var i = 0; i < nums.Length; i++)
+        {
+            if (map.ContainsValue(nums[i]))
+                return true;
+            else
+                map.Add(i, nums[i]);
+        }
+
+        return false;
+    }
+
+    public bool ContainsDuplicate_v2(int[] nums)
+    {
+        var exisitingItems = new HashSet<int>();
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            if (exisitingItems.Contains(nums[i]))
+                return true;
+            else
+                exisitingItems.Add(nums[i]);
+        }
+
+        return false;
+    }
+}
---
+++ DSA/Array/ContainsDuplicate.js
@@ -0,0 +1,17 @@
+﻿/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function (nums) {
+
+    const existingItems = new Set();
+
+    for (let num of nums) {
+        if (existingItems.has(num)) {
+            return true;
+        }
+        existingItems.add(num);
+    }
+
+    return false;
+};
